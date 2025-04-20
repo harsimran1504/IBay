ebd982cf579f55c2cc3f87c1b914b5feda4a2054
Bonbonpoa <104199921+Bonbonpoa@users.noreply.github.com>
2025-04-20 21:05:08 +0100

    Tweaks

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+$host = "your-db-host";        // e.g., "db.uni.edu" or an IP
+$user = "your-db-username";
+$pass = "your-db-password";
+$dbname = "your-database-name";
+echo "<h1>Register</h1>";
+
+
+?>
