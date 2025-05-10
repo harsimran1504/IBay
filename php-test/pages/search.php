15aba46596575d32042efe353f51e4d82a8ff793
Bonbonpoa <104199921+Bonbonpoa@users.noreply.github.com>
2025-05-10 14:36:33 +0100

    Change to styles2
    Update on styles

--- php-test/pages/search.php
+++ php-test/pages/search.php
@@ -21,7 +21,7 @@ $query = trim($query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Results</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/styles2.css">
 </head>
 <body>
     <div class="NavButtons">
